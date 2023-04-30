038163feaf1583da3b09c6512cb8a7d2e55b16f9
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-05-01 03:22:36 +0600

    email phone verify trait logic updated

--- app/Traits/EmailPhoneVerifyTrait.php
+++ app/Traits/EmailPhoneVerifyTrait.php
@@ -6,18 +6,15 @@ trait EmailPhoneVerifyTrait
 {
     public function syncVerifiedDate()
     {
-        if (isset($this->email)) {
             if ($this->email == null || strlen($this->email) < 5) {
                 $this->email_verified_at = null;
             }
-        }
 
-        if (isset($this->phone)) {
             if ($this->phone == null || strlen($this->phone) < 8) {
                 $this->phone_verified_at = null;
             }
-        }
 
         $this->getDirty();
+
     }
 }

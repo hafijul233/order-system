d004ef3fb294bc607257f4c91f94b13b374ba612
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-06-30 23:46:56 +0600

    Translation issue fixed

--- app/Http/Controllers/Admin/SettingCrudController.php
+++ app/Http/Controllers/Admin/SettingCrudController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Illuminate\Support\Facades\Config;
 
 class SettingCrudController extends CrudController
 {
@@ -13,12 +14,14 @@ class SettingCrudController extends CrudController
     public function setup()
     {
         CRUD::setModel("App\Models\Setting");
-        CRUD::setEntityNameStrings(trans('backpack::settings.setting_singular'), trans('backpack::settings.setting_plural'));
+        CRUD::setEntityNameStrings(trans('settings.setting_singular'), trans('settings.setting_plural'));
         CRUD::setRoute(backpack_url(config('backpack.settings.route')));
     }
 
     public function setupListOperation()
     {
+        CRUD::setOperationSetting('lineButtonsAsDropdown', false);
+        
         // only show settings which are marked as active
         CRUD::addClause('where', 'active', 1);
 
@@ -26,15 +29,15 @@ class SettingCrudController extends CrudController
         CRUD::setColumns([
             [
                 'name'  => 'name',
-                'label' => trans('backpack::settings.name'),
+                'label' => trans('settings.name'),
             ],
             [
                 'name'  => 'value',
-                'label' => trans('backpack::settings.value'),
+                'label' => trans('settings.value'),
             ],
             [
                 'name'  => 'description',
-                'label' => trans('backpack::settings.description'),
+                'label' => trans('settings.description'),
             ],
         ]);
     }
@@ -43,7 +46,7 @@ class SettingCrudController extends CrudController
     {
         CRUD::addField([
             'name'       => 'name',
-            'label'      => trans('backpack::settings.name'),
+            'label'      => trans('settings.name'),
             'type'       => 'text',
             'attributes' => [
                 'disabled' => 'disabled',

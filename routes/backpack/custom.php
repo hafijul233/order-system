29c0f327d35792e723a81e978465a7729fe3c318
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-07-01 15:38:34 +0600

    installation verify middleware added

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -12,7 +12,8 @@ Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => array_merge(
         (array) config('backpack.base.web_middleware', 'web'),
-        (array) config('backpack.base.middleware_key', 'admin')
+        (array) config('backpack.base.middleware_key', 'admin'),
+        ['installed']
     ),
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
